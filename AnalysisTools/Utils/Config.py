19321a1ad5b2e01eae3228e6037c8da60d8bcf82
jejeffre <jdavi231@jhu.edu>
2022-02-08 19:04:57 +0100

    Fix the Utils paths/ function definitions etc

--- AnalysisTools/Utils/Config.py
+++ AnalysisTools/Utils/Config.py
@@ -8,4 +8,5 @@ class Analysis_Config:
       self.TaggingProcess = "Tag_AC_19_Scheme_2"
       self.ReweightProcess = "Calc_Event_Weight_2021_gammaH"
       self.TreeFile = "200205_CutBased"
+      self.Save_Failed = True
       self.Path_To_Utils = "/afs/cern.ch/work/j/jejeffre/public/HEP_Ex_Tools/HepExUtils/OnShell_Utils"
